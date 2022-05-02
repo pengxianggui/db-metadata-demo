aead71fa02c766ef027a615fc1349bb8a9958fe9
pengxg <pengxianggui@outlook.com>
2022-05-02 22:06:08 +0800

    update

--- web/src/main.js
+++ web/src/main.js
@@ -2,18 +2,19 @@ import Vue from 'vue'
 import DbMeta from 'db-metadata'
 import ElementUI from 'element-ui'
 
+// css
+import 'element-ui/lib/theme-chalk/index.css'
+import 'db-metadata/lib/db-metadata.css'
+
 import App from './App.vue'
 import router from './router'
 
 import DbMetaConf from './dbmeta.config'
 
-// css
-import 'element-ui/lib/theme-chalk/index.css'
-import 'db-metadata/lib/db-metadata.css'
 
 Vue.use(ElementUI)
-
 Vue.use(DbMeta, DbMetaConf)
+
 new Vue({
   router,
   render: h => h(App),
