0fa016e2f4e8cf7e26a70946c84d2cc38aea6489
pengxg <pengxianggui@outlook.com>
2022-10-12 13:05:59 +0800

    update: 调试并应用dbmeta-2.2.2-beta版本;

---
+++ server/src/test/java/com/hthjsj/md/demo/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.hthjsj.md.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author pengxg
+ * @date 2022/10/10 8:49 上午
+ */
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
