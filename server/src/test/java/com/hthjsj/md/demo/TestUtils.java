9b43fe3e85830510f5bcc711799347929e870199
pengxg <pengxianggui@outlook.com>
2022-11-14 11:01:36 +0800

    update

--- server/src/test/java/com/hthjsj/md/demo/TestUtils.java
+++ server/src/test/java/com/hthjsj/md/demo/TestUtils.java
@@ -1,15 +1,10 @@
 package com.hthjsj.md.demo;
 
-import com.alibaba.fastjson.JSONObject;
-import com.github.md.analysis.SpringAnalysisManager;
-import com.jfinal.plugin.activerecord.Record;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.text.StringEscapeUtils;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 /**
  * @author pengxg
@@ -17,28 +12,6 @@ import java.util.Date;
  */
 @Slf4j
 public class TestUtils {
-
-    @Test
-    public void test() {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("key1", "1");
-        jsonObject.put("key2", 2);
-
-        JSONObject value3 = new JSONObject();
-        value3.put("key3-1", new Date());
-        value3.put("key3-2", "jlsjdljs");
-
-        JSONObject value33 = new JSONObject();
-        value33.put("key3-3-1", "hello world");
-        value3.put("key3-2", value33);
-        jsonObject.put("key3", value3);
-
-        log.info("json: {}", jsonObject.toJSONString());
-        log.info("json: {}", jsonObject);
-
-        log.info("json: {}", StringEscapeUtils.escapeJson(jsonObject.toJSONString()));
-        log.info("json: {}", jsonObject.toString());
-    }
 
     @Test
     public void testReplace() {
