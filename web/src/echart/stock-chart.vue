ea9cf2860e9f003c368d33232d66379212d3c15f
pengxg <pengxianggui@outlook.com>
2024-08-12 18:09:35 +0800

    update

---
+++ web/src/echart/stock-chart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "stock-chart"
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ web/src/views/dashboard/dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-container">
+    <stock-log-chart></stock-log-chart>
+    <stock-chart></stock-chart>
+  </div>
+</template>
+
+<script>
+import StockLogChart from "@/echart/stock-log-chart.vue";
+import StockChart from "@/echart/stock-chart.vue";
+
+export default {
+  name: "dashboard",
+  components: {StockChart, StockLogChart}
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
