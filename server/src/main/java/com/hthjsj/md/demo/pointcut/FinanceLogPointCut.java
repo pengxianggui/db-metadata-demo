4776508a84ff5f95a98449d2c176be094ef173b3
pengxg <pengxianggui@outlook.com>
2024-10-18 10:32:38 +0800

    update

--- server/src/main/java/com/hthjsj/md/demo/pointcut/FinanceLogPointCut.java
+++ server/src/main/java/com/hthjsj/md/demo/pointcut/FinanceLogPointCut.java
@@ -1,10 +1,15 @@
 package com.hthjsj.md.demo.pointcut;
 
+import com.github.md.analysis.AnalysisSpringUtil;
 import com.github.md.analysis.SpringAnalysisManager;
+import com.github.md.analysis.meta.BusinessService;
+import com.github.md.analysis.meta.IMetaObject;
 import com.github.md.analysis.meta.MetaData;
-import com.github.md.analysis.meta.aop.AddPointCut;
-import com.github.md.analysis.meta.aop.FormInvocation;
-import com.github.md.analysis.meta.aop.UpdatePointCut;
+import com.github.md.analysis.meta.aop.*;
+import com.github.md.web.controller.TableQueryInvocation;
+import com.google.common.collect.ImmutableMap;
+import com.hthjsj.md.demo.service.StockLogService;
+import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
 
 import java.math.BigDecimal;
@@ -14,7 +19,7 @@ import java.math.BigDecimal;
  * @author pengxg
  * @date 2024/8/5 21:37
  */
-public class FinanceLogPointCut implements AddPointCut, UpdatePointCut {
+public class FinanceLogPointCut implements AddPointCut, UpdatePointCut, TableQueryPointCut {
     @Override
     public boolean addBefore(FormInvocation invocation) {
         Long stockLogId = invocation.getFormData().getLong("stock_log_id");
@@ -28,4 +33,19 @@ public class FinanceLogPointCut implements AddPointCut, UpdatePointCut {
         return AddPointCut.super.addBefore(invocation);
     }
 
+    /**
+     * 列表查询后干预返回结果。将关联的出入库记录附带进来
+     *
+     * @param queryInvocation
+     */
+    @Override
+    public boolean queryAfter(QueryInvocation queryInvocation) {
+        Page<Record> result = ((TableQueryInvocation) queryInvocation).getData();
+        result.getList().forEach(r -> {
+            Long stockLogId = r.getLong("stock_log_id");
+            Record record = AnalysisSpringUtil.getBean(StockLogService.class).getStockLog(stockLogId);
+            r.set("stock_log", record);
+        });
+        return true;
+    }
 }
